95d9abcaed600de7c0b7e871fde8f4c7abf4aaee
scarletcamomile <scarletcamomile@mail.ru>
2018-10-05 18:00:09 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let baseArr = [];
+  for (let i = 2; i <= base; i++) {
+    let count = 0
+    if (base % i == 0) {
+      while (base % i == 0) {
+        count++;
+        base /= i;
+      }
+      baseArr.push({
+        'val': i,
+        'count': count
+      });
+    }
+  }
+
+  return Math.min.apply(null, baseArr.map((val) => {
+    return ({
+      'val': findPower(val.val, number),
+      'count': val.count
+    });
+  }).map((val) => {
+    return Math.floor(val.val /= val.count)
+  }))
+
+  function findPower (val, count) {
+    let ext = 0,
+    power = 1;
+    for (;;) {
+      power *= val;
+      let addend = Math.floor(count / power);
+      if (addend < 1)
+        return ext;
+      ext += addend;
+    }
+  }
 }
